8c1e9b46d09d02872096040a597bb7a72d13c05d
Weiwei <93496699+wei858910@users.noreply.github.com>
2025-08-01 15:06:24 +0800

    Update to UE 5.6

--- Source/AuraEditor.Target.cs
+++ Source/AuraEditor.Target.cs
@@ -8,8 +8,9 @@ public class AuraEditorTarget : TargetRules
 	public AuraEditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
+		
 		ExtraModuleNames.AddRange( new string[] { "Aura" } );
 	}
 }
